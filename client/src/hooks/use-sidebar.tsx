8a44491085619892af8a4c3510562d29ad3f0e0b
MUZAMMIL ISHTIAQ <tomuzammilishtiaq@gmail.com>
2026-02-06 11:28:08 +0500

    isMobileview add

--- client/src/hooks/use-sidebar.tsx
+++ client/src/hooks/use-sidebar.tsx
@@ -1,22 +1,22 @@
 import { create } from 'zustand';
 
-// Create a simple store for sidebar state
 interface SidebarState {
   isOpen: boolean;
+  isMobileView: boolean;
   toggle: () => void;
   open: () => void;
   close: () => void;
+  checkScreenSize: () => void;
 }
 
-// Create the store with zustand (a lightweight, no dependency state manager)
 const useSidebarStore = create<SidebarState>((set) => ({
   isOpen: true,
-  toggle: () => {
-    console.log("Zustand toggle called");
-    set((state) => ({ isOpen: !state.isOpen }));
-  },
+  isMobileView: false,
+  toggle: () => set((state) => ({ isOpen: !state.isOpen })),
   open: () => set({ isOpen: true }),
   close: () => set({ isOpen: false }),
+  checkScreenSize: () =>
+    set({ isMobileView: window.innerWidth < 768 }),
 }));
 
 export default useSidebarStore;
